86b9fe9bd1139a0ccd6b144e996ac6196acea3c9
oluzr <olu-zr@naver.com>
2023-12-04 14:35:15 +0900

    docs : Modify file structure (1)

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,33 +1,33 @@
-"use client";
-import { checkUser } from "@/api/user";
-import { useEffect, useLayoutEffect } from "react";
-import { useRecoilState } from "recoil";
-import { UserState, UserState as userstate } from "@/store/user";
-import Link from "next/link";
+'use client'
+import { checkUser } from '@/api/user'
+import { useEffect, useLayoutEffect } from 'react'
+import { useRecoilState } from 'recoil'
+import { UserState, UserState as userstate } from '@/store/user'
+import Link from 'next/link'
 const Avatar = ({
   width,
   height,
   isItprofile,
 }: {
-  width?: string;
-  height?: string;
-  isItprofile?: boolean;
+  width?: string
+  height?: string
+  isItprofile?: boolean
 }) => {
-  const [user, setUser] = useRecoilState(UserState);
+  const [user, setUser] = useRecoilState(UserState)
   useEffect(() => {
     checkUser().then((resp) => {
-      setUser(resp);
-    });
-  }, [setUser]);
+      setUser(resp)
+    })
+  }, [setUser])
   return (
     <div style={{ width: width, height: height }}>
       <div className="avatar_img">
-        <Link href={isItprofile ? "#none" : "/profile"}>
+        <Link href={isItprofile ? '#none' : '/profile'}>
           <img src={user.profileImage} alt="profileImage" />
         </Link>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Avatar;
+export default Avatar
--- src/components/records.tsx
+++ src/components/records.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { getUsersRecords } from "@/api/records";
 import { RecordsState } from "@/store/records";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useRecoilState } from "recoil";
@@ -13,17 +12,7 @@ const Records = () => {
       setRecords(resp.records);
     });
   }, [setRecords]);
-  if (!records) {
-    return (
-      <ul>
-        <li>
-          <div className="thumbnail" style={{ padding: "10px" }}>
-            <div className="skeleton h-52"></div>
-          </div>
-        </li>
-      </ul>
-    );
-  }
+
   return (
     <ul>
       {records.map((record) => {
