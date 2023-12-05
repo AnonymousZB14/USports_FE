201df9817d7ab766bf64ff0e14ee2c71f3e15cac
seon-o <seon.o.min@gmail.com>
2023-12-05 14:36:10 +0900

    feat: 모집글 작성 html DaumPostcodeEmbed Api 구현

---
+++ src/components/Button.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import { MouseEventHandler, ReactNode } from "react";
+import { UrlObject } from "url";
+
+interface Props {
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  height?: "fit" | "unset";
+  tailwindStyles?: string;
+  theme?: string;
+  href?: UrlObject | string;
+  disabled?: boolean;
+}
+const Button: React.FC<Props> = ({
+  children,
+  onClick,
+  height = "fit",
+  tailwindStyles,
+  theme = "gray",
+  href,
+  disabled = false,
+}) => {
+  return href ? (
+    <Link
+      scroll={false}
+      href={href}
+      className={`inline-block w-fit ${
+        height === "unset" ? "" : "h-fit"
+      } break-keep rounded-md px-4 py-2 text-center text-base font-semibold transition-all ${
+        theme === "gray"
+        ? "bg-zinc-200 text-zinc-600 hover:bg-zinc-100"
+        : theme === "black"
+        ? "bg-zinc-800 text-zinc-50 hover:bg-zinc-500"
+        : theme === "red"
+        ? "bg-red-600 text-zinc-50 hover:bg-red-400"
+        : theme === "rose"
+        ? "bg-orange-600 text-zinc-50 hover:bg-orange-500"
+        : theme === "blue"
+        ? "bg-blue-700 text-zinc-50 hover:bg-blue-600"
+        : ""
+      } ${
+        disabled
+          ? "!pointer-events-none !bg-zinc-100 !text-zinc-200"
+          : "!pointer-events-auto"
+      } ${tailwindStyles}`}
+    >
+      {children}
+    </Link>
+  ) : (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`inline-block w-fit ${
+        height === "unset" ? "" : "h-fit"
+      } break-keep rounded-md px-4 py-2 text-center text-base font-semibold transition-all ${
+        theme === "gray"
+        ? "bg-zinc-200 text-zinc-600 hover:bg-zinc-100"
+        : theme === "black"
+        ? "bg-zinc-800 text-zinc-50 hover:bg-zinc-500"
+        : theme === "red"
+        ? "bg-red-600 text-zinc-50 hover:bg-red-400"
+        : theme === "orange"
+        ? "bg-orange-600 text-zinc-50 hover:bg-orange-400"
+        : theme === "blue"
+        ? "bg-blue-700 text-zinc-50 hover:bg-blue-600"
+        : ""
+      } ${
+        disabled
+          ? "!pointer-events-none !bg-zinc-100 !text-zinc-200"
+          : "!pointer-events-auto"
+      } ${tailwindStyles}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/addressForm.tsx
@@ -0,0 +1,118 @@
+'use client'
+import {
+  ChangeEvent,
+  Dispatch,
+  MouseEvent,
+  SetStateAction,
+  useState,
+} from "react";
+import DaumPostcodeEmbed, { Address } from "react-daum-postcode";
+import Button from "./Button";
+import { AddressType } from "../../types";
+
+interface Props {
+  addressData: AddressType | null;
+  setAddressData: Dispatch<SetStateAction<AddressType | null>>;
+}
+
+const FormAddress: React.FC<Props> = ({ addressData, setAddressData }) => {
+  const [showSearch, setShowSearch] = useState<boolean>(false);
+
+  const onAdditionalAddressChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const { value } = e.target;
+    setAddressData((prev) => ({
+      address: prev?.address || "",
+      postCode: prev?.postCode || "",
+      additional: value,
+    }));
+  };
+
+  const onAddressSearchToggle = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setShowSearch((prev) => !prev);
+  };
+
+  const onAddressSearchComplete = (data: Address) => {
+    setAddressData({
+      address: data.address || "",
+      postCode: data.zonecode || "",
+      additional: "",
+    });
+
+    setShowSearch(false);
+  };
+
+  const onAddressDelete = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    setAddressData(null);
+  };
+
+  return (
+    <div>
+      <div
+        className={`mx-2 flex flex-col flex-wrap gap-2 ${
+          !addressData && "text-zinc-500"
+        }`}
+      >
+        <div className="flex gap-2">
+          <Button onClick={onAddressSearchToggle} tailwindStyles="py-0 px-2">
+            {showSearch ? "창 닫기" : "주소 검색"}
+          </Button>
+          {addressData && (
+            <Button onClick={onAddressDelete} tailwindStyles="py-0 px-2">
+              지우기
+            </Button>
+          )}
+        </div>
+        <DaumPostcodeEmbed
+          style={{
+            height: showSearch ? "500px" : "0px",
+            border: showSearch ? "1px solid #71717a" : "none",
+            borderRadius: "15px",
+            overflow: "hidden",
+          }}
+          onComplete={onAddressSearchComplete}
+          autoClose={false}
+        />
+        <div className="flex flex-col gap-2">
+          <span>
+            {addressData?.address
+              ? `(${addressData?.postCode})`
+              : addressData
+              ? `(${addressData.postCode})`
+              : ""}{" "}
+            {addressData?.address
+              ? addressData?.address
+              : addressData
+              ? addressData.address
+              : "검색된 주소 없음"}
+          </span>
+          {addressData && (
+            <input
+              placeholder="추가 주소"
+              value={addressData?.additional || ""}
+              onChange={onAdditionalAddressChange}
+              style={{
+                borderBottom: "1px solid #1f2937",
+              }}
+              className="h-8 w-[200px] px-2 pt-1 pb-1"
+            />
+          )}
+        </div>
+        <div className="flex gap-2">
+          <Button tailwindStyles="py-0 px-2" theme="gray">
+              작성취소
+          </Button>
+          <Button tailwindStyles="py-0 px-2" theme="blue">
+            작성완료
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormAddress;
