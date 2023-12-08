2cd9e883b3b1b182b76d1ca8f1e8bae19d80c750
oluzr <olu-zr@naver.com>
2023-12-08 16:59:15 +0900

    design : modify P layout

--- src/app/(not-logged)/login/page.tsx
+++ src/app/(not-logged)/login/page.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
-      Login
+      <h2>Log into USports</h2>
+      <form>
+        <input type="email" name="email" id="email" placeholder='email' required/>
+        <input type="password" name="password" id="password" placeholder='password' required/>
+        <input type="submit" value="Log in" />
+      </form>
+      <div className='LinkWrap'>
+        <Link href={'/'}>비밀번호 찾기</Link>
+        <Link href={'/'}>회원가입</Link>
+      </div>
+      <hr />
+      <div className='kakaoLogBtn'></div>
     </div>
   );
 };
