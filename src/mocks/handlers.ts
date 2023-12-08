6132f08836a1d5078de5b3606785ca5c2559472f
oluzr <olu-zr@naver.com>
2023-12-08 20:38:52 +0900

    feat : msw setting

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,25 @@
+import { http, HttpResponse, StrictResponse } from 'msw'
+
+const User = [
+  { id: 'elonmusk', nickname: 'Elon Musk', image: '/yRsRRjGO.jpg' },
+  { id: 'zerohch0', nickname: '제로초', image: '/5Udwvqim.jpg' },
+]
+
+export const handlers = [
+  http.post('/api/login', () => {
+    console.log('로그인')
+    return HttpResponse.json(User[1], {
+      headers: {
+        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
+      },
+    })
+  }),
+  http.post('/api/logout', () => {
+    console.log('로그아웃')
+    return new HttpResponse(null, {
+      headers: {
+        'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0',
+      },
+    })
+  }),
+]
