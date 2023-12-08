e8de3290f8d68b35e21f00b0aed2b4e2edcc643c
oluzr <olu-zr@naver.com>
2023-12-08 20:08:27 +0900

    design : modify layout

--- src/app/(not-logged)/layout.tsx
+++ src/app/(not-logged)/layout.tsx
@@ -12,7 +12,7 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html>
+    <html data-theme="light">
       <body>{children}</body>
     </html>
   )
