5c32adef08594402e8dd8e0e07bbaff55e591f61
oluzr <olu-zr@naver.com>
2023-12-11 05:53:59 +0900

    feat : next auth

--- src/mocks/http.ts
+++ src/mocks/http.ts
@@ -5,14 +5,14 @@ import { handlers } from './handlers'
 
 const app = express()
 const port = 9090
+
 app.use(
   cors({
-    origin: 'http//localhost:3001',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200,
     credentials: true,
   }),
 )
 app.use(express.json())
 app.use(createMiddleware(...handlers))
-
-app.listen(port, () => console.log(`Mock server is running on port : ${port}`))
+app.listen(port, () => console.log(`Mock server is running on port: ${port}`))
