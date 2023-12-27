d001bcae5be9a2c0a9fdb8b868e14ee6292adb40
oluzr <olu-zr@naver.com>
2023-12-27 15:20:45 +0900

    fix : error

--- next.config.js
+++ next.config.js
@@ -12,6 +12,23 @@ const nextConfig = {
     return [
       {
         // matching all API routes
+        source: 'http://3.39.34.245:8080/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+      {
+        // matching all API routes
         source: '/usports/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
