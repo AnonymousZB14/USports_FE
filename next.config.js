a2957ec784872db5a6d256ac7c0cab3de2b6c4d8
oluzr <olu-zr@naver.com>
2023-12-27 15:08:16 +0900

    fix : cors error

--- next.config.js
+++ next.config.js
@@ -2,11 +2,32 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  generateStaticParams: function () {
+  async generateStaticParams() {
     return {
       '/': { page: '/' },
       'src/app/(logged)/@modal/(.)member/:id': { page: '/member/:id' }, // correct
     }
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
   },
   async rewrites() {
     return [
