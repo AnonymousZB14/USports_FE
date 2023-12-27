748f6a70f360c91222a7860ed3a33c88de6e2a08
oluzr <olu-zr@naver.com>
2023-12-27 15:22:34 +0900

    fix : error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   async generateStaticParams() {
     return {
-      '/': { page: '/' },
       'src/app/(logged)/@modal/(.)member/:id': { page: '/member/:id' }, // correct
     }
   },
@@ -13,23 +12,6 @@ const nextConfig = {
       {
         // matching all API routes
         source: 'http://3.39.34.245:8080/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-      {
-        // matching all API routes
-        source: '/usports/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
