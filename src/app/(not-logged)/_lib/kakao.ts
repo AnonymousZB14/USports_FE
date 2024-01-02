a4e0a49522fedfaa9b731ab9ab9eef058cb21cef
oluzr <olu-zr@naver.com>
2024-01-02 12:45:22 +0900

    feat : kakao

--- src/app/(not-logged)/_lib/kakao.ts
+++ src/app/(not-logged)/_lib/kakao.ts
@@ -1,6 +1,3 @@
-const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY
-// const KAKAO_REDIRECT_URI = 'http://localhost:3000/login/kakao'
-const KAKAO_REDIRECT_URI = 'http://localhost:3000/oauth2/authorization/kakao'
-
-// 이 URL이 카카오 버튼을 클릭했을 때 이동하는 URL
+const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY //제 거
+const KAKAO_REDIRECT_URI = 'http://3.39.34.245:8080/oauth2/authorization/kakao'
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&scope=account_email`
